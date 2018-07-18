5206f5b50da00fa4a0ac0f7d7bb4640faee4dc26
Dawson Wilson <dawsonw72@gmail.com>
2018-07-18 17:01:51 -0300

    Added some blank function and some default values

--- hospital.c
+++ hospital.c
@@ -3,8 +3,31 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(){
+#define BUFFER_SIZE 20
 
+static int queue[BUFFER_SIZE];
 
-return 0;
+void *doctor(void* parameters)
+{
+    // I'm a doctor I will wait for patients and then will serve the most recently arriving patient
+
+    return NULL;
+}
+
+void *patient(void* parameters)
+{
+    // I'm a patient I will wait in the lobby until the doctor is read to server me
+
+    return NULL;
+}
+
+int main()
+{
+    
+    pthread_t tid[2]; 
+
+    pthread_create(&tid[0], NULL, doctor, NULL); 
+    pthread_create(&tid[1], NULL, patient, NULL);
+
+    return 0;
 }
