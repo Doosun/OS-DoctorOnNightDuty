fcbd426c9f2bad706dc16aa469da88591fd05f22
Dawson Wilson <dawsonw72@gmail.com>
2018-07-21 14:35:00 -0300

    added a lock and a queue, not integrated yet

--- hospital.c
+++ hospital.c
@@ -2,15 +2,79 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
 
-#define BUFFER_SIZE 3
+#define QUEUE_SIZE 3
 
-static int queue[BUFFER_SIZE];
+pthread_mutex_t lock;
+struct Queue* queue;
 
-struct patient {
+// A structure to represent a queue
+struct Queue
+{
+    int front, rear, size;
+    unsigned capacity;
+    struct Patient* array;
+};
+
+struct Patient {
     int id;
     int visits;
 };
+ 
+// function to create a queue of given capacity. 
+// It initializes size of queue as 0
+struct Queue* createQueue(unsigned capacity)
+{
+    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue));
+    queue->capacity = capacity;
+    queue->front = queue->size = 0; 
+    queue->rear = capacity - 1;  // This is important, see the enqueue
+    queue->array = malloc(queue->capacity * sizeof(struct Patient*));
+    return queue;
+}
+ 
+// Queue is full when size becomes equal to the capacity 
+int isFull(struct Queue* queue)
+{  return (queue->size == queue->capacity);  }
+ 
+// Queue is empty when size is 0
+int isEmpty(struct Queue* queue)
+{  return (queue->size == 0); }
+ 
+// Function to add an item to the queue.  
+// It changes rear and size
+void enqueue(struct Queue* queue, struct Patient patient)
+{
+    // not needex i think
+    /*if (isFull(queue))
+        return;*/
+    queue->rear = (queue->rear + 1)%queue->capacity;
+    queue->array[queue->rear] = patient;
+    queue->size = queue->size + 1;
+    printf("patient %d waiting for doctor\n", patient.id);
+}
+ 
+// Function to remove an item from queue. 
+// It changes front and size
+struct Patient dequeue(struct Queue* queue)
+{
+    // no needed
+    /*if (isEmpty(queue))
+        return INT_MIN;*/
+    struct Patient patient = queue->array[queue->front];
+    queue->front = (queue->front + 1)%queue->capacity;
+    queue->size = queue->size - 1;
+    return patient;
+}
+ 
+// Function to get front of queue
+/*struct Patient* front(struct Queue* queue)
+{
+    if (isEmpty(queue))
+        return ;
+    return queue->array[queue->front];
+}*/
 
 void *doctor(void* parameters)
 {
@@ -21,22 +85,36 @@ void *doctor(void* parameters)
 
 void *patient(void* parameters)
 {
-    struct patient *p = parameters;
+    struct Patient *p = parameters;
     printf("Patient with id [%d] needs [%d] visits\n", p->id, p->visits);
-    /*
-    while (needs more visits) {
-        if (space in queue) {
-            sit in queue
-        } else {
-            wait in lobby and have coffee
-        }
+
+    while (p->visits > 0)
+    {
+        // lock
+        pthread_mutex_lock(&lock);
+
+        // check queue space
+            // enqueue if room
+        // else go drink coffee
+        // unlock
+        pthread_mutex_unlock(&lock);
     }
-    */
+    
     return NULL;
 }
 
 int main()
 {
+
+    if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init has failed\n");
+        return 1;
+    }
+
+    // create queue
+    queue = createQueue(QUEUE_SIZE);
+
     time_t t;
 
     /* initializes a random number generator with the current time as the seed */
@@ -46,7 +124,7 @@ int main()
     int number_of_patients = 10 + (rand() % 11); 
 
     /* creates the array of patients */
-    struct patient patients[number_of_patients];
+    struct Patient patients[number_of_patients];
     
     /* 1 doctor pthread, and the patient pthreads */
     pthread_t tid[1 + number_of_patients]; 
@@ -87,6 +165,9 @@ int main()
     }
 
     // TODO check from the return status of the threads (tid array)
+
+
+    pthread_mutex_destroy(&lock);
 
     return 0;
 }
